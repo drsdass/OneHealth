dff1ad01173039e54eefc52e726b98756fc26339
drsdass <satishsdass@gmail.com>
2025-06-09 21:11:19 -0700

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,31 @@
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from .models import User
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .auth.routes import auth_bp
+    from .main.routes import main_bp
+    from .admin.routes import admin_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+    app.register_blueprint(admin_bp)
+
+    return app
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
