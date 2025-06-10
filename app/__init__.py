7c2bb6b12db3d1035bce400df7302a0f06da4558
drsdass <satishsdass@gmail.com>
2025-06-09 21:40:00 -0700

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_moment import Moment
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+moment = Moment()
 
 def create_app():
     app = Flask(__name__)
@@ -12,10 +14,16 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    moment.init_app(app)
 
-    from .routes import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    # Import blueprints
+    from .auth.routes import auth_bp
+    from .main.routes import main_bp
+    from .admin.routes import admin_bp
 
-    from .models import User  # Imported after db is defined
+    # Register blueprints
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+    app.register_blueprint(admin_bp)
 
     return app
