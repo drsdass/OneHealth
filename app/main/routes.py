2da7f37774caf2e7a3a339d891179ea6c53e34c6
drsdass <satishsdass@gmail.com>
2025-06-09 20:57:58 -0700

    Add files via upload

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,12 @@
-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 from app.utils.admin_access_util import get_admin_entities
 
 main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    return redirect(url_for('auth.login'))
 
 @main_bp.route('/dashboard')
 @login_required
