9c84587c57ede394ea84d573e4c027ccde2621b3
drsdass <satishsdass@gmail.com>
2025-06-09 21:51:32 -0700

    Add files via upload

---
+++ models (3).py
@@ -0,0 +1,8 @@
+from flask_login import UserMixin
+from app import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default='user')
